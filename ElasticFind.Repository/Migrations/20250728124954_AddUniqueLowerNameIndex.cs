3046ca4056a4234b364d6b0fcda299f8a79cd73f
Shubham Pandya <shubham.pandya@tatvasoft.com>
2025-07-28 18:36:36 +0530

    Add Unique Lower Name Constraint to DB

---
+++ ElasticFind.Repository/Migrations/20250728124954_AddUniqueLowerNameIndex.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElasticFind.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueLowerNameIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "CREATE UNIQUE INDEX unique_lower_name ON categories (LOWER('categories.Name'));");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "DROP INDEX unique_lower_name;");
+        }
+    }
+}
